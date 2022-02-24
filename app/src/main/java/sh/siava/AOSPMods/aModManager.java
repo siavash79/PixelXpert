be486490e7545606833880dcb3afd6c65549a4dd
Siavash <siavash@gmail.com>
2022-02-24 17:07:58 +0330

    Mod Classes Main Structure made

---
+++ app/src/main/java/sh/siava/AOSPMods/aModManager.java
@@ -0,0 +1,14 @@
+package sh.siava.AOSPMods;
+
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+abstract class aModManager {
+    protected XC_LoadPackage.LoadPackageParam lpparam;
+
+    aModManager(XC_LoadPackage.LoadPackageParam lpparam) {
+//        this.getClass().getConstructor(String.class);
+        this.lpparam = lpparam;
+    }
+
+    protected abstract void hookMethods();
+}

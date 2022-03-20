2238f002354e107e95b10631957fb35beb03a5b5
Siavash <siavash@gmail.com>
2022-03-20 23:33:28 +0330

    Misc

--- app/src/main/java/sh/siava/AOSPMods/AOSPMods.java
+++ app/src/main/java/sh/siava/AOSPMods/AOSPMods.java
@@ -4,22 +4,7 @@ import java.util.ArrayList;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
-import sh.siava.AOSPMods.systemui.BackGestureManager;
-import sh.siava.AOSPMods.systemui.BackToKill;
-import sh.siava.AOSPMods.systemui.BatteryStyleManager;
-import sh.siava.AOSPMods.systemui.CarrierTextManager;
-import sh.siava.AOSPMods.systemui.DoubleTapSleepLS;
-import sh.siava.AOSPMods.systemui.FeatureFlagsMods;
-import sh.siava.AOSPMods.systemui.KeyguardBottomArea;
-import sh.siava.AOSPMods.systemui.LTEiconChange;
-import sh.siava.AOSPMods.systemui.NavBarResizer;
-import sh.siava.AOSPMods.systemui.QSFooterTextManager;
-import sh.siava.AOSPMods.systemui.QSHaptic;
-import sh.siava.AOSPMods.systemui.QSHeaderManager;
-import sh.siava.AOSPMods.systemui.QSQuickPullDown;
-import sh.siava.AOSPMods.systemui.ScreenshotController;
-import sh.siava.AOSPMods.systemui.StatusbarMods;
-import sh.siava.AOSPMods.systemui.UDFPSManager;
+import sh.siava.AOSPMods.systemui.*;
 
 public class AOSPMods implements IXposedHookLoadPackage{
 

426c5759ccf869af25d80fbf0e30f25481341b7e
Siavash <siavash@gmail.com>
2022-03-27 22:08:13 +0430

    fix carrier text not showing bug

--- app/src/main/java/sh/siava/AOSPMods/systemui/CarrierTextManager.java
+++ app/src/main/java/sh/siava/AOSPMods/systemui/CarrierTextManager.java
@@ -1,6 +1,7 @@
 package sh.siava.AOSPMods.systemui;
 
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 import sh.siava.AOSPMods.IXposedModPack;
@@ -22,21 +23,23 @@ public class CarrierTextManager implements IXposedModPack {
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
         if(!lpparam.packageName.equals(listenPackage)) return;
 
-        XposedHelpers.findAndHookConstructor("com.android.keyguard.CarrierTextManager$CarrierTextCallbackInfo", lpparam.classLoader,
-                CharSequence.class, CharSequence[].class, boolean.class, int[].class, boolean.class, new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param)  {
-                        if(isEnabled)
-                        {
-                            XposedHelpers.setObjectField(param.thisObject, "carrierText", customText);
-                            XposedHelpers.setObjectField(param.thisObject, "listOfCarriers", new CharSequence[0]);
-                            XposedHelpers.setObjectField(param.thisObject, "anySimReady", true);
-                            XposedHelpers.setObjectField(param.thisObject, "subscriptionIds", new int[0]);
-                            XposedHelpers.setObjectField(param.thisObject, "airplaneMode", false);
-                            param.setResult(null);
-                        }
-                    }
-                });
+        Class CarrierTextCallbackInfo = XposedHelpers.findClass("com.android.keyguard.CarrierTextManager$CarrierTextCallbackInfo", lpparam.classLoader);
+
+        XposedBridge.hookAllConstructors(CarrierTextCallbackInfo, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                if(isEnabled)
+                {
+                    XposedHelpers.setObjectField(param.thisObject, "carrierText", customText);
+                    XposedHelpers.setObjectField(param.thisObject, "listOfCarriers", new CharSequence[0]);
+                    XposedHelpers.setObjectField(param.thisObject, "anySimReady", true);
+                    XposedHelpers.setObjectField(param.thisObject, "subscriptionIds", new int[0]);
+                    XposedHelpers.setObjectField(param.thisObject, "airplaneMode", false);
+                    param.setResult(null);
+                }
+            }
+        });
+
     }
 
     @Override

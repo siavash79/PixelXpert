f4e77466b4e10bbfa6cf2d0dca4a64d8b9e8cd0d
Siavash <siavash@gmail.com>
2022-03-27 22:22:19 +0430

    Misc + Version bump to 007

--- app/src/main/java/sh/siava/AOSPMods/SettingsActivity.java
+++ app/src/main/java/sh/siava/AOSPMods/SettingsActivity.java
@@ -17,7 +17,7 @@ import com.topjohnwu.superuser.Shell;
 
 import sh.siava.AOSPMods.Utils.Helpers;
 import sh.siava.AOSPMods.Utils.Overlays;
-import sh.siava.AOSPModss.R;
+
 
 public class SettingsActivity extends AppCompatActivity implements
         PreferenceFragmentCompat.OnPreferenceStartFragmentCallback {

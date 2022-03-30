04b81f56218e05bafe8f70861544af7712637571
Siavash <siavash@gmail.com>
2022-03-30 22:13:13 +0430

    Misc settings improvement

--- app/src/main/java/sh/siava/AOSPMods/systemui/miscSettings.java
+++ app/src/main/java/sh/siava/AOSPMods/systemui/miscSettings.java
@@ -2,6 +2,7 @@ package sh.siava.AOSPMods.systemui;
 
 import com.topjohnwu.superuser.Shell;
 
+import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 import sh.siava.AOSPMods.IXposedModPack;
 import sh.siava.AOSPMods.XPrefs;
@@ -10,32 +11,80 @@ public class miscSettings implements IXposedModPack {
 
     @Override
     public void updatePrefs(String...Key) {
-        if(Key == null)
+        if(XPrefs.Xprefs == null) return; //it won't be null. but anyway...
+
+        if(Key.length == 0)
         {
             //we are at startup
+            updateSysUITuner();
+            updateWifiCell();
         }
         else
         {
             //we know what has changed
-        }
-        // here you can ask for preferences and do stuff based on them.
-        //preferences are located here: XPrefs.Xprefs
-        Boolean SysUITunerEnabled = XPrefs.Xprefs.getBoolean("sysui_tuner", false);
-        if(SysUITunerEnabled)
-        {
-            Shell.su("pm enable com.android.systemui/.tuner.TunerActivity").exec();
-        } else {
-            Shell.su("pm disable com.android.systemui/.tuner.TunerActivity").exec();
-        }
-
-        Boolean WifiCellEnabled = XPrefs.Xprefs.getBoolean("wifi_cell", false);
-        if(SysUITunerEnabled)
-        {
-            Shell.su("settings put global settings_provider_model false && settings list secure | grep sysui_qs_tiles > /sdcard/current_qs_tiles.txt && sed 's/wifi,cell,//g' /sdcard/current_qs_tiles.txt > /sdcard/nowificell_qs_tiles.txt && sed 's/=/ /g' /sdcard/nowificell_qs_tiles.txt > /sdcard/new_qs_tiles.txt && sed 's/sysui_qs_tiles/settings put secure sysui_qs_tiles/g' /sdcard/new_qs_tiles.txt > /sdcard/new2_qs_tiles.txt && sed -re 's/\\(/\"\\(/g' /sdcard/new2_qs_tiles.txt > /sdcard/new3_qs_tiles.txt && sed -re 's/\\)/\\)\"/g' /sdcard/new3_qs_tiles.txt > /sdcard/new4_qs_tiles.txt && sed -re 's/internet,/wifi,cell,/g' /sdcard/new4_qs_tiles.txt > /sdcard/final_qs_tiles.txt && sh /sdcard/final_qs_tiles.txt && rm -rf /sdcard/current_qs_tiles.txt && rm -rf /sdcard/new_qs_tiles.txt && rm -rf /sdcard/new2_qs_tiles.txt && rm -rf /sdcard/new3_qs_tiles.txt && rm -rf /sdcard/new4_qs_tiles.txt && rm -rf /sdcard/final_qs_tiles.txt && rm -rf /sdcard/nowificell_qs_tiles.txt").exec();
-        } else {
-            Shell.su("settings list secure | grep sysui_qs_tiles > /sdcard/current_qs_tiles.txt && sed 's/internet,//g' /sdcard/current_qs_tiles.txt > /sdcard/nointernet_qs_tiles.txt && sed 's/=/ /g' /sdcard/nointernet_qs_tiles.txt > /sdcard/new_qs_tiles.txt && sed 's/sysui_qs_tiles/settings put secure sysui_qs_tiles/g' /sdcard/new_qs_tiles.txt > /sdcard/new2_qs_tiles.txt && sed -re 's/\\(/\"\\(/g' /sdcard/new2_qs_tiles.txt > /sdcard/new3_qs_tiles.txt && sed -re 's/\\)/\\)\"/g' /sdcard/new3_qs_tiles.txt > /sdcard/new4_qs_tiles.txt && sed -re 's/wifi,cell,/internet,/g' /sdcard/new4_qs_tiles.txt > /sdcard/final_qs_tiles.txt && sh /sdcard/final_qs_tiles.txt && rm -rf /sdcard/current_qs_tiles.txt && rm -rf /sdcard/new_qs_tiles.txt && rm -rf /sdcard/new2_qs_tiles.txt && rm -rf /sdcard/new3_qs_tiles.txt && rm -rf /sdcard/new4_qs_tiles.txt && rm -rf /sdcard/final_qs_tiles.txt && rm -rf /sdcard/nointernet_qs_tiles.txt && settings put global settings_provider_model true").exec();
+            switch(Key[0])
+            {
+                case "sysui_tuner":
+                    updateSysUITuner();
+                    break;
+                case "wifi_cell":
+                    updateWifiCell();
+                    break;
+            }
         }
     }
+
+    private void updateWifiCell() {
+        Boolean WifiCellEnabled = XPrefs.Xprefs.getBoolean("wifi_cell", false);
+
+        try {
+            String currentTiles = Shell.su("settings get secure sysui_qs_tiles").exec().getOut().get(0);
+
+            boolean hasWifi = currentTiles.contains("wifi");
+            boolean hasCell = currentTiles.contains("cell");
+            boolean hasInternet = currentTiles.contains("internet");
+
+            boolean providerModel;
+            if (WifiCellEnabled) {
+                providerModel = false;
+                if (!hasCell) {
+                    currentTiles = "cell," + currentTiles;
+                }
+                if (!hasWifi) {
+                    currentTiles = "wifi," + currentTiles;
+                }
+                currentTiles = currentTiles.replace(",,", ",");
+            } else {
+                providerModel = true;
+
+                currentTiles = currentTiles.replace("wifi", "").replace(",,", ",").replace("cell", "").replace(",,", ",");
+                if (!hasInternet) {
+                    currentTiles = "internet," + currentTiles;
+                }
+                currentTiles = currentTiles.replace(",,", ",");
+
+            }
+
+            if (currentTiles.startsWith(",")) {
+                currentTiles = currentTiles.substring(1, currentTiles.length() - 1);
+            }
+            // sorry I was toooo lazy to go through regex work..... this seems good enough
+            XposedBridge.log(currentTiles);
+
+            XposedBridge.log("settings put global settings_provider_model " + providerModel + "; settings put secure sysui_qs_tiles \"" + currentTiles + "\"");
+            Shell.su("settings put global settings_provider_model " + providerModel + "; settings put secure sysui_qs_tiles \"" + currentTiles + "\"").exec();
+        }catch (Exception ignored){}
+    }
+
+    private void updateSysUITuner() {
+        Boolean SysUITunerEnabled = XPrefs.Xprefs.getBoolean("sysui_tuner", false);
+        String mode = (SysUITunerEnabled) ? "enable" : "disable";
+
+        try {
+            Shell.su("pm " + mode + " com.android.systemui/.tuner.TunerActivity").exec();
+        }catch(Exception ignored){}
+    }
+
 
     @Override
     public String getListenPack() {

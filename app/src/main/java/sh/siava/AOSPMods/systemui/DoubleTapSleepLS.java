702ae3e335af517eb11f972035a461718512f985
Siavash <siavash@gmail.com>
2022-04-05 08:54:26 +0430

    fix implementation of Double tap to sleep

--- app/src/main/java/sh/siava/AOSPMods/systemui/DoubleTapSleepLS.java
+++ app/src/main/java/sh/siava/AOSPMods/systemui/DoubleTapSleepLS.java
@@ -15,72 +15,75 @@ import sh.siava.AOSPMods.IXposedModPack;
 import sh.siava.AOSPMods.XPrefs;
 
 public class DoubleTapSleepLS implements IXposedModPack {
-    public static final String listenPackage = "com.android.systemui";
-    public static boolean doubleTapToSleepEnabled = false;
-
-    final Context[] context = new Context[1];
-
-    public void updatePrefs(String...Key)
-    {
-        if(XPrefs.Xprefs == null) return;
-        doubleTapToSleepEnabled = XPrefs.Xprefs.getBoolean("DoubleTapSleep", false);
-    }
-
-    @Override
-    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
-        if(!lpparam.packageName.equals(listenPackage)) return;
-
-        GestureDetector mLockscreenDoubleTapToSleep = new GestureDetector(context[0], new GestureDetector.SimpleOnGestureListener() {
-            @Override
-            public boolean onDoubleTap(MotionEvent e) {
-
-                PowerManager pm = (PowerManager) context[0].getSystemService(Context.POWER_SERVICE);
-                if (pm != null) {
-                    XposedHelpers.callMethod(pm, "goToSleep", SystemClock.uptimeMillis());
-                }
-                return true;
-            }
-        });
-
-        Class NotificationPanelViewControllerClass = XposedHelpers.findClass("com.android.systemui.statusbar.phone.NotificationPanelViewController", lpparam.classLoader);
-
-        XposedHelpers.findAndHookMethod("com.android.systemui.statusbar.phone.NotificationPanelViewController", lpparam.classLoader,
-                "createTouchHandler", new XC_MethodHook() {
-                    @Override
-                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                        Object touchHandler = param.getResult();
-                        boolean mPulsing = (boolean) XposedHelpers.getObjectField(param.thisObject, "mPulsing");
-                        boolean mDozing = (boolean) XposedHelpers.getObjectField(param.thisObject, "mDozing");
-                        int mBarState = (int) XposedHelpers.getObjectField(param.thisObject, "mBarState");
-
-                        XposedHelpers.findAndHookMethod((Class)touchHandler.getClass(),
-                                "onTouch", View.class, MotionEvent.class, new XC_MethodHook() {
-                                    @Override
-                                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                                        if (doubleTapToSleepEnabled && !mPulsing && !mDozing
-                                                && mBarState == 0) {
-                                            mLockscreenDoubleTapToSleep.onTouchEvent((MotionEvent) param.args[1]);
-                                        }
-                                    }
-                                });
-                    }
-                });
-
-
-        XposedBridge.hookAllConstructors(NotificationPanelViewControllerClass,
-                new XC_MethodHook() {
-                    @Override
-                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                        Object mView = XposedHelpers.getObjectField(param.thisObject, "mView");
-                        context[0] = (Context) XposedHelpers.callMethod(mView, "getContext");
-                    }
-                });
-
-    }
-
-    @Override
-    public String getListenPack() {
-        return listenPackage;
-    }
-
+	public static final String listenPackage = "com.android.systemui";
+	public static boolean doubleTapToSleepEnabled = false;
+	
+	final Context[] context = new Context[1];
+	
+	public void updatePrefs(String...Key)
+	{
+		if(XPrefs.Xprefs == null) return;
+		doubleTapToSleepEnabled = XPrefs.Xprefs.getBoolean("DoubleTapSleep", false);
+	}
+	
+	@Override
+	public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
+		if(!lpparam.packageName.equals(listenPackage)) return;
+		
+		Class NotificationPanelViewControllerClass = XposedHelpers.findClass("com.android.systemui.statusbar.phone.NotificationPanelViewController", lpparam.classLoader);
+		
+		GestureDetector mLockscreenDoubleTapToSleep = new GestureDetector(context[0], new GestureDetector.SimpleOnGestureListener() {
+			@Override
+			public boolean onDoubleTap(MotionEvent e) {
+				
+				Object pm = (PowerManager) context[0].getSystemService(Context.POWER_SERVICE);
+				if (pm != null) {
+					XposedHelpers.callMethod(pm, "goToSleep", SystemClock.uptimeMillis());
+				}
+				return true;
+			}
+		});
+		
+		XposedHelpers.findAndHookMethod("com.android.systemui.statusbar.phone.NotificationPanelViewController", lpparam.classLoader,
+				"createTouchHandler", new XC_MethodHook() {
+					@Override
+					protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+						Object touchHandler = param.getResult();
+						
+						XposedHelpers.findAndHookMethod(touchHandler.getClass(),
+								"onTouch", View.class, MotionEvent.class, new XC_MethodHook() {
+									@Override
+									protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+										if(!doubleTapToSleepEnabled) return;
+										
+										boolean mPulsing = (boolean) XposedHelpers.getObjectField(param.thisObject, "mPulsing");
+										boolean mDozing = (boolean) XposedHelpers.getObjectField(param.thisObject, "mDozing");
+										int mBarState = (int) XposedHelpers.getObjectField(param.thisObject, "mBarState");
+										
+										if (!mPulsing && !mDozing
+												&& mBarState == 0) {
+											mLockscreenDoubleTapToSleep.onTouchEvent((MotionEvent) param.args[1]);
+										}
+									}
+								});
+					}
+				});
+		
+		
+		XposedBridge.hookAllConstructors(NotificationPanelViewControllerClass,
+				new XC_MethodHook() {
+					@Override
+					protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+						Object mView = XposedHelpers.getObjectField(param.thisObject, "mView");
+						context[0] = (Context) XposedHelpers.callMethod(mView, "getContext");
+					}
+				});
+		
+	}
+	
+	@Override
+	public String getListenPack() {
+		return listenPackage;
+	}
+	
 }

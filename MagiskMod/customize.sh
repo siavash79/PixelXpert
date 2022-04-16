d89ff62897b7c85d10f32001c93800628f782499
Siavash <siavash@gmail.com>
2022-04-16 09:18:27 +0430

    updated install script to grant root to required apps

--- MagiskMod/customize.sh
+++ MagiskMod/customize.sh
@@ -1,48 +1,100 @@
-DBPATH="/data/adb/lspd/config/modules_config.db"
-SQLITEPATH="$TMPDIR/sqlite3"
 PKGNAME="sh.siava.AOSPMods"
 PKGPATH="/system/priv-app/AOSPMods/AOSPMods.apk"
+LSPDDBPATH="/data/adb/lspd/config/modules_config.db"
+MAGISKDBPATH="/data/adb/magisk.db"
 
-activate_module()
-{
+prepareSQL(){
 	unzip $ZIPFILE sqlite3 -d $TMPDIR/ > /dev/null
 	chmod 777 $TMPDIR/sqlite3
-	
-	ui_print 'Checking activation status in Lsposed...'	
-	OLDMID=$($SQLITEPATH $DBPATH "select mid from modules where module_pkg_name like \"$PKGNAME\";" | xargs)
 
-	if [ $(($OLDMID+0)) = 0 ]; then
-		PRESENTINDB=false
-	else
-		PRESENTINDB=true
-		REALMID=$($SQLITEPATH $DBPATH "select mid from modules where mid = $OLDMID and apk_path like \"$PKGPATH\" and enabled = 1;" | xargs)
+	SQLITEPATH="$TMPDIR/sqlite3"
+}
+
+# runSQL "database path" "command" - then you can use $SQLRESULT to read the outcome
+runSQL(){
+	SQLRESULT=$($SQLITEPATH $DBPATH "$CMD")
+}
+
+#grant silent root access to given UID
+grantRootUID(){
+	DBPATH=$MAGISKDBPATH
+	
+	#new record - older magisk compatibility
+	CMD="insert into policies (uid, package_name, policy, until, logging, notification) values ($1, '$2', 2, 0, 1, 0);" && runSQL
+	#new record
+	CMD="insert into policies (uid, policy, until, logging, notification) values ($1, 2, 0, 1, 0);" && runSQL
+	#previously present record
+	CMD="update policies set policy = 2, until = 0, logging = 1, notification = 0 where uid = $1;" && runSQL
+}
+
+
+#grant root access to given package name
+grantRootPkg(){
+	ui_print "- 	Granting root access to $1..."
+	UID=$(pm list packages -U $1 --user 0 | grep ":$1 " | awk -F 'uid:' '{ print $2 }')
+
+	grantRootUID $UID $1
+}
+
+#grant root access to required apps
+grantRootApps(){
+	grantRootPkg "com.android.systemui" 
+	grantRootPkg $PKGNAME
+}
+
+#activate PKGNAME in Lsposed
+activateModuleLSPD()
+{	
+	DBPATH=$LSPDDBPATH
+	
+	ui_print '- Trying to activate the module in Lsposed...'	
+	
+	CMD="select mid from modules where module_pkg_name like \"$PKGNAME\";" && runSQL
+	OLDMID=$(echo $SQLREUSLT | xargs)
+
+
+	if [ $(($OLDMID+0)) > 0 ]; then
+		CMD="select mid from modules where mid = $OLDMID and apk_path like \"$PKGPATH\" and enabled = 1;" && runSQL
+		REALMID=$(echo $SQLREUSLT | xargs)
+		
 		if [ $(($REALMID+0)) = 0 ]; then
-			$($SQLITEPATH $DBPATH "delete from scope where mid = $OLDMID;")
-			$($SQLITEPATH $DBPATH "delete from modules where mid = $OLDMID;")		
+			CMD="delete from scope where mid = $OLDMID;" && runSQL
+			CMD="delete from modules where mid = $OLDMID;" && runSQL
 		fi
 	fi
 	
-	ui_print 'Trying to activate the module in Lsposed...'	
-
 #some commands may fail. It's OK if they do	
-	$($SQLITEPATH $DBPATH "insert into modules (\"module_pkg_name\", \"apk_path\", \"enabled\") values (\"$PKGNAME\",\"$PKGPATH\", 1);")
-	NEWMID=$($SQLITEPATH $DBPATH "select mid as ss from modules where module_pkg_name = \"$PKGNAME\";")
+	CMD="insert into modules (\"module_pkg_name\", \"apk_path\", \"enabled\") values (\"$PKGNAME\",\"$PKGPATH\", 1);" && runSQL
+	
+	CMD="select mid as ss from modules where module_pkg_name = \"$PKGNAME\";" && runSQL
+	
+	NEWMID=$(echo $SQLRESULT | xargs)
 
-	$($SQLITEPATH $DBPATH "insert into scope (mid, app_pkg_name, user_id) values ($NEWMID, \"com.android.systemui\",0);")
-	$($SQLITEPATH $DBPATH "insert into scope (mid, app_pkg_name, user_id) values ($NEWMID, \"com.google.android.apps.nexuslauncher\",0);")
-#	$($SQLITEPATH $DBPATH "insert into scope (mid, app_pkg_name, user_id) values ($NEWMID, \"android\",0);")
-	$($SQLITEPATH $DBPATH "insert into scope (mid, app_pkg_name, user_id) values ($NEWMID, \"$PKGNAME\",0);")
-
+	CMD="insert into scope (mid, app_pkg_name, user_id) values ($NEWMID, \"com.android.systemui\",0);" && runSQL
+	CMD="insert into scope (mid, app_pkg_name, user_id) values ($NEWMID, \"com.google.android.apps.nexuslauncher\",0);" && runSQL
+	CMD="insert into scope (mid, app_pkg_name, user_id) values ($NEWMID, \"$PKGNAME\",0);" && runSQL
 }
 
 rm -f "$MODPATH/sqlite3"
 
+prepareSQL
 
-if [ $(ls $DBPATH) = $DBPATH ]; then
-	activate_module
+ui_print ''
+ui_print ''
+
+grantRootApps
+
+if [ $(ls $LSPDDBPATH) = $LSPDDBPATH ]; then
+	ui_print ''
+	ui_print ''
+
+	activateModuleLSPD
 	
+	ui_print ''
+	ui_print ''
 	ui_print 'Installation Complete!'
-	ui_print 'Please Reboot your device <2 times!> to activate'
+	ui_print 'Please Reboot your device to activate'
+	ui_print '(Activation of additional fonts may take one more reboot)'
 else
 	ui_print 'Lspsed not found!!'
 	ui_print 'This module will not work without Lsposed'
@@ -51,8 +103,14 @@ else
 	ui_print '- Reboot'
 	ui_print '- Manually enable AOSPMods in Lsposed'
 	ui_print '- Reboot'
-
-	exit
 fi
 
-
+	ui_print ''
+	ui_print '  **********************'
+	ui_print '  * Brought to you by: *'
+	ui_print '  *                    *'
+	ui_print '  * siavash79@github   *'
+	ui_print '  *                    *'
+	ui_print '  * eltifo@github      *'
+	ui_print '  **********************'
+	ui_print ''

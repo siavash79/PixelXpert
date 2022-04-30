8fc162caea512b9c548306a0617a130c67e4c072
Siavash <siavash@gmail.com>
2022-04-30 14:58:10 +0430

    update to canary 13

--- makeMagisk.bat
+++ makeMagisk.bat
@@ -2,6 +2,6 @@ cp app\release\AOSPMods.apk MagiskMod\system\priv-app\AOSPMods
 
 cd MagiskMod
 
-zip -r ..\AOSPMods.zip *.*
+zip -r -9 -q ..\AOSPMods.zip *.*
 
 rm -Rf system\priv-app\AOSPMods\AOSPMods.apk

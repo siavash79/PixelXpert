7e66e3ab9ff6ddbdf964d628db7b4e09a0d4412c
Siavash <siavash@gmail.com>
2022-05-01 12:19:02 +0430

    github actions

--- .github/make_zip.sh
+++ .github/make_zip.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 mkdir output
-cd MagiskMod || exit;
+cp app/build/outputs/apk/release/AOSPMods-signed.apk MagiskMod/system/priv-app/AOSPMods/AOSPMods.apk
+cd MagiskMod;
 zip -r ../output/AOSPMods.zip *;

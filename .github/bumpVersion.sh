3efb1ddfcb3dbfa4c41bc91631e711a93cf5e046
Siavash <siavash@gmail.com>
2022-05-01 15:01:36 +0430

    modify bumpVersion.sh

--- .github/bumpVersion.sh
+++ .github/bumpVersion.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-echo $(pwd)
-
-echo $(ls)
-
-NEWVERCODE=$(($(cat build.gradle | grep versionCode | tr -s ' ' |cut -d " " -f 3 | tr -d '\r')+1))
+NEWVERCODE=$(($(cat app/build.gradle | grep versionCode | tr -s ' ' |cut -d " " -f 3 | tr -d '\r')+1))
 NEWVERNAME=${GITHUB_REF_NAME/v/}
 
-sed -i 's/versionCode.*/versionCode '$NEWVERCODE'/' build.gradle
-sed -i 's/versionName.*/versionName "'$NEWVERNAME'"/' build.gradle
+sed -i 's/versionCode.*/versionCode '$NEWVERCODE'/' app/build.gradle
+sed -i 's/versionName.*/versionName "'$NEWVERNAME'"/' app/build.gradle
 
 sed -i 's/version=.*/version='$NEWVERNAME'/' MagiskMod/module.prop
 sed -i 's/versionCode=.*/versionCode='$NEWVERCODE'/' MagiskMod/module.prop

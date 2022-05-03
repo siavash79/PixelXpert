d141fad441540e83c6799c52c37bf8cc3027fb66
Siavash <siavash@gmail.com>
2022-05-04 01:10:51 +0430

    package test build scripts

--- .github/make_debug_zip.sh
+++ .github/make_debug_zip.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 mkdir output
-cd MagiskMod;
-echo $(ls app/build/outputs/apk/debug)
 cp app/build/outputs/apk/debug/AOSPMods-signed.apk MagiskMod/system/priv-app/AOSPMods/AOSPMods.apk
+cd MagiskMod
 FILENAME="AOSPMods.zip"
 zip -r ../output/$FILENAME *;

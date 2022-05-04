56f39a52c67ac923d498b4d34a63a6e3a95b7bc2
Siavash <siavash@gmail.com>
2022-05-04 21:38:29 +0430

    debug screenOffKeys

--- app/src/main/java/sh/siava/AOSPMods/android/screenOffKeys.java
+++ app/src/main/java/sh/siava/AOSPMods/android/screenOffKeys.java
@@ -32,11 +32,9 @@ public class screenOffKeys implements IXposedModPack {
     private static boolean holdVolumeToSkip = false;
     private CameraManager cameraManager = null;
     private long wakeTime = 0;
-    private Context mContext;
     private AudioManager audioManager = null;
 //    private boolean isVolumeLongPress = false;
     private boolean isVolDown = false;
-    private Handler mHandler = null;
     private PowerManager powerManager = null;
 
     CameraManager.TorchCallback torchCallback = new CameraManager.TorchCallback() {
@@ -57,10 +55,12 @@ public class screenOffKeys implements IXposedModPack {
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam, Context context) throws Throwable {
         if (!lpparam.packageName.equals(listenPackage)) return;
         
-        mContext = context;
-
+        audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+        cameraManager = (CameraManager) context.getSystemService(Context.CAMERA_SERVICE);
+        cameraManager.registerTorchCallback(torchCallback, new Handler());
+        powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        
         Class<?> PhoneWindowManager;
-        Method init;
         Method powerLongPress;
         Method startedWakingUp;
         Method interceptKeyBeforeQueueing;
@@ -68,7 +68,6 @@ public class screenOffKeys implements IXposedModPack {
         try {
             PhoneWindowManager = XposedHelpers.findClass("com.android.server.policy.PhoneWindowManager", lpparam.classLoader);
             powerLongPress = XposedHelpers.findMethodExact(PhoneWindowManager, "powerLongPress", long.class);
-            init = XposedHelpers.findMethodExact(PhoneWindowManager, "init", Context.class, "android.view.IWindowManager", "com.android.server.policy.WindowManagerPolicy.WindowManagerFuncs");
             startedWakingUp = XposedHelpers.findMethodExact(PhoneWindowManager, "startedWakingUp", int.class);
             interceptKeyBeforeQueueing = XposedHelpers.findMethodExact(PhoneWindowManager, "interceptKeyBeforeQueueing", KeyEvent.class, int.class);
         }
@@ -93,7 +92,9 @@ public class screenOffKeys implements IXposedModPack {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                 if(!holdVolumeToSkip) return;
-
+    
+                Handler mHandler = (Handler) XposedHelpers.getObjectField(param.thisObject, "mHandler");
+                
                 try {
                     KeyEvent e = (KeyEvent) param.args[0];
                     int Keycode = e.getKeyCode();
@@ -118,40 +119,22 @@ public class screenOffKeys implements IXposedModPack {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                 if(!replaceAssistantwithTorch) return;
-
                 int r = (int) param.args[0];
-
+                
                 if(r==1)
                 {
                     wakeTime = Calendar.getInstance().getTimeInMillis();
                 }
             }
         });
-
-        XposedBridge.hookMethod(init, new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                try
-                {
-                    mHandler = (Handler) XposedHelpers.getObjectField(param.thisObject, "mHandler");
-                    audioManager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
-                    cameraManager = (CameraManager) mContext.getSystemService(Context.CAMERA_SERVICE);
-                    cameraManager.registerTorchCallback(torchCallback, new Handler());
-                    powerManager = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
-                }
-                catch (Throwable T)
-                {
-                    T.printStackTrace();
-                }
-            }
-        });
-
+        
 
         XposedBridge.hookMethod(powerLongPress, new XC_MethodHook() {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                 if(!replaceAssistantwithTorch) return;
                 if(Calendar.getInstance().getTimeInMillis() - wakeTime > 1000) return;
+    
                 try {
                     int behavior = (int) XposedHelpers.callMethod(param.thisObject, "getResolvedLongPressOnPowerBehavior");
 
@@ -183,7 +166,7 @@ public class screenOffKeys implements IXposedModPack {
             {
                 return;
             }
-
+            
             cameraManager.setTorchMode(flashID, !torchOn);
         }
         catch(Throwable T)

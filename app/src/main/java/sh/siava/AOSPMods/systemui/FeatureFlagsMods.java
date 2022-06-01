37ffbc190aa537e21fd59854280fb1effca3246b
Siavash <siavash@gmail.com>
2022-06-01 13:45:01 +0430

    Combined FeatureFlags and LTE Icon flags

--- app/src/main/java/sh/siava/AOSPMods/systemui/FeatureFlagsMods.java
+++ app/src/main/java/sh/siava/AOSPMods/systemui/FeatureFlagsMods.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Build;
 
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 import sh.siava.AOSPMods.AOSPMods;
@@ -13,8 +14,14 @@ import sh.siava.AOSPMods.XPrefs;
 public class FeatureFlagsMods extends XposedModPack {
     public static final String listenPackage = AOSPMods.SYSTEM_UI_PACKAGE;
 
+    private static final int SIGNAL_DEFAULT = 0;
+    private static final int SIGNAL_FORCE_LTE = 1;
+    private static final int SIGNAL_FORCE_4G = 2;
+
+    public static int SBLTEIcon = SIGNAL_DEFAULT;
+
     public static boolean combinedSignalEnabled = false;
-    
+
     public FeatureFlagsMods(Context context) { super(context); }
 
     @Override
@@ -30,11 +37,29 @@ public class FeatureFlagsMods extends XposedModPack {
             }catch(Exception ignored){}
         }
         combinedSignalEnabled = newcombinedSignalEnabled;
+
+        SBLTEIcon = Integer.parseInt(XPrefs.Xprefs.getString(
+                "LTE4GIconMod",
+                String.valueOf(SIGNAL_DEFAULT)));
+
     }
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         if(!lpparam.packageName.equals(listenPackage)) return;
+
+        XposedBridge.hookAllMethods(
+                XposedHelpers.findClass("com.android.settingslib.mobile.MobileMappings$Config", lpparam.classLoader),
+                "readConfig", new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        if(SBLTEIcon == SIGNAL_DEFAULT) return;
+
+                        XposedHelpers.setObjectField(param.getResult(),
+                                "show4gForLte",
+                                SBLTEIcon == SIGNAL_FORCE_4G);
+                    }
+                });
 
         if(Build.VERSION.SDK_INT < 32) return; //Feature flags is newly introduced!
 
@@ -50,5 +75,4 @@ public class FeatureFlagsMods extends XposedModPack {
 
     @Override
     public boolean listensTo(String packageName) { return listenPackage.equals(packageName); }
-
 }

5ac47edc616ab3ee12bec80ef25a3be77d18569e
Siavash <siavash@gmail.com>
2022-06-07 16:54:11 +0430

    CHANGELOG: Added changelog for updates

--- .github/extractChanges.sh
+++ .github/extractChanges.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-IFS=$'\n'
-
 rm -f changeLog.md
 touch changeLog.md
 LASTUPDATE=$(git log -100 | grep -B 4 "Version update: Release" | grep "commit" -m 1 | cut -d " " -f 2)
 COMMITS=$(git rev-list $LASTUPDATE..HEAD)
+
+IFS=$'\n'
 for COMMIT in $COMMITS
 do
         FULLCOMMIT=$(git show $COMMIT | grep -zoP "CHANGELOG: .+" | tr -d '\0')

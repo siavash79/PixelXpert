8b33b67dc2384bcf3ff9d47a4d120f75d96922b3
Siavash <siavash@gmail.com>
2022-06-07 15:54:24 +0400

    Update extractChanges.sh

--- .github/extractChanges.sh
+++ .github/extractChanges.sh
@@ -3,6 +3,7 @@
 IFS=$'\n'
 
 rm -f changeLog.md
+touch changeLog.md
 LASTUPDATE=$(git log -100 | grep -B 4 "Version update: Release" | grep "commit" -m 1 | cut -d " " -f 2)
 COMMITS=$(git rev-list $LASTUPDATE..HEAD)
 for COMMIT in $COMMITS

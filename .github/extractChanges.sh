d703af4dcf0bd8c6c3f73624a34bfa478db135f1
Siavash <siavash@gmail.com>
2022-06-08 00:04:13 +0430

    fix changelog extractor

--- .github/extractChanges.sh
+++ .github/extractChanges.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-rm -f changeLog.md
-touch changeLog.md
-LASTUPDATE=$(git log -100 | grep -B 4 "Version update: Release" | grep "commit" -m 1 | cut -d " " -f 2)
-COMMITS=$(git rev-list $LASTUPDATE..HEAD)
+  # reset the file - most likely not needd
+  rm -f changeLog.md
+  touch changeLog.md
 
-IFS=$'\n'
-for COMMIT in $COMMITS
-do
-        FULLCOMMIT=$(git show $COMMIT | grep -zoP "CHANGELOG: .+" | tr -d '\0')
-        for LINE in $FULLCOMMIT
-        do
-                 echo "- "${LINE##*CHANGELOG: } >> changeLog.md
-        done
-done
+  #find the last time we made a changelog
+  LASTUPDATE=$(git log -100 | grep -B 4 "Version update: Release" | grep "commit" -m 1 | cut -d " " -f 2)
+  #find commits since - starting with the magic phrase
+  COMMITS=$(git rev-list $LASTUPDATE..HEAD --grep "^CHANGELOG: ")
+
+  #separator is newline
+  IFS=$'\n'
+  for COMMIT in $COMMITS
+  do
+    COMMITMSGS=$(git show $COMMIT --pretty=format:"%s" | grep "^CHANGELOG: " | tr -d '\0')
+      for LINE in $COMMITMSGS
+      do
+        #save in the temp file to be used by next script
+        echo "- "${LINE##*CHANGELOG: } >> changeLog.md
+      done
+  done

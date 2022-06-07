410f270672a30c61234fa5de6c7d287ac825de24
Siavash <siavash@gmail.com>
2022-06-07 15:37:36 +0400

    Create extractChanges.sh

---
+++ .github/extractChanges.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+IFS=$'\n'
+
+cd ..
+rm changeLog.md
+LASTUPDATE=$(git log -100 | grep -B 4 "Version update: Release" | grep "commit" -m 1 | cut -d " " -f 2)
+COMMITS=$(git rev-list $LASTUPDATE..HEAD)
+for COMMIT in $COMMITS
+do
+        FULLCOMMIT=$(git show $COMMIT | grep -zoP "CHANGELOG: .+" | tr -d '\0')
+        for LINE in $FULLCOMMIT
+        do
+                 echo "- "${LINE##*CHANGELOG: } >> changeLog.md
+        done
+done

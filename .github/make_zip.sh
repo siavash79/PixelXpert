482ec1de3ea99259ff99c68c0c719d81e731c4b9
Siavash <siavash@gmail.com>
2022-07-15 00:47:57 +0430

    Update release scripts

--- .github/make_zip.sh
+++ .github/make_zip.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-mkdir -p output/full output/xposed
+mkdir -p output
 cp app/build/outputs/apk/release/AOSPMods-signed.apk MagiskModBase/system/priv-app/AOSPMods/AOSPMods.apk
 cd MagiskModBase;
 LITE_FILENAME="AOSPMods_Xposed.zip"
 FULL_FILENAME="AOSPMods_Full.zip"
 
 echo 1 > build.type
-zip -r ../output/xposed/$LITE_FILENAME *;
+zip -r ../output/$LITE_FILENAME *;
 
-cp -r ../MagiskModAddon/* ./
+cp -rf ../MagiskModAddon/* ./
 echo 0 > build.type
 
-zip -r ../output/full/$FULL_FILENAME *;
+zip -r ../output/$FULL_FILENAME *;

10e79d738ccbfcbfa479f627e5058165ec816bff
Siavash <siavash@gmail.com>
2022-07-15 23:59:12 +0430

    Update build scripts

--- .github/bumpVersion.sh
+++ .github/bumpVersion.sh
@@ -8,28 +8,31 @@ sed -i 's/versionCode.*/versionCode '$NEWVERCODE'/' app/build.gradle
 sed -i 's/versionName.*/versionName "'$NEWVERNAME'"/' app/build.gradle
 
 #prepare magisk module props
+#Xposed
 sed -i 's/version=.*/version='$NEWVERNAME'/' MagiskModBase/module.prop
 sed -i 's/versionCode=.*/versionCode='$NEWVERCODE'/' MagiskModBase/module.prop
-
+#Full
 sed -i 's/version=.*/version='$NEWVERNAME'/' MagiskModAddon/module.prop
 sed -i 's/versionCode=.*/versionCode='$NEWVERCODE'/' MagiskModAddon/module.prop
 
 #Prepare magisk module update files of legacy, full and xposed types
+#legacy
 sed -i 's/"version":.*/"version": "'$NEWVERNAME'",/' MagiskModuleUpdate.json
 sed -i 's/"versionCode":.*/"versionCode": '$NEWVERCODE',/' MagiskModuleUpdate.json
 sed -i 's/"zipUrl":.*/"zipUrl": "https:\/\/github.com\/siavash79\/AOSPMods\/releases\/download\/'$GITHUB_REF_NAME'\/AOSPMods_Full.zip",/' MagiskModuleUpdate.json
-
+#Full
 sed -i 's/"version":.*/"version": "'$NEWVERNAME'",/' MagiskModuleUpdate_Full.json
 sed -i 's/"versionCode":.*/"versionCode": '$NEWVERCODE',/' MagiskModuleUpdate_Full.json
 sed -i 's/"zipUrl":.*/"zipUrl": "https:\/\/github.com\/siavash79\/AOSPMods\/releases\/download\/'$GITHUB_REF_NAME'\/AOSPMods_Full.zip",/' MagiskModuleUpdate_Full.json
-sed -i 's/"zipUrl_FUll":.*/"zipUrl": "https:\/\/github.com\/siavash79\/AOSPMods\/releases\/download\/'$GITHUB_REF_NAME'\/AOSPMods_Full.zip",/' MagiskModuleUpdate_Full.json
-sed -i 's/"zipUrl_Xposed":.*/"zipUrl": "https:\/\/github.com\/siavash79\/AOSPMods\/releases\/download\/'$GITHUB_REF_NAME'\/AOSPMods_Xposed.zip",/' MagiskModuleUpdate_Full.json
-
+#Xposed
 sed -i 's/"version":.*/"version": "'$NEWVERNAME'",/' MagiskModuleUpdate_Xposed.json
 sed -i 's/"versionCode":.*/"versionCode": '$NEWVERCODE',/' MagiskModuleUpdate_Xposed.json
 sed -i 's/"zipUrl":.*/"zipUrl": "https:\/\/github.com\/siavash79\/AOSPMods\/releases\/download\/'$GITHUB_REF_NAME'\/AOSPMods_Xposed.zip",/' MagiskModuleUpdate_Xposed.json
-sed -i 's/"zipUrl_FUll":.*/"zipUrl": "https:\/\/github.com\/siavash79\/AOSPMods\/releases\/download\/'$GITHUB_REF_NAME'\/AOSPMods_Full.zip",/' MagiskModuleUpdate_Xposed.json
-sed -i 's/"zipUrl_Xposed":.*/"zipUrl": "https:\/\/github.com\/siavash79\/AOSPMods\/releases\/download\/'$GITHUB_REF_NAME'\/AOSPMods_Xposed.zip",/' MagiskModuleUpdate_Xposed.json
+#In-app updater
+sed -i 's/"version":.*/"version": "'$NEWVERNAME'",/' latestStable.json
+sed -i 's/"versionCode":.*/"versionCode": '$NEWVERCODE',/' latestStable.json
+sed -i 's/"zipUrl_FUll":.*/"zipUrl": "https:\/\/github.com\/siavash79\/AOSPMods\/releases\/download\/'$GITHUB_REF_NAME'\/AOSPMods_Full.zip",/' latestStable.json
+sed -i 's/"zipUrl_Xposed":.*/"zipUrl": "https:\/\/github.com\/siavash79\/AOSPMods\/releases\/download\/'$GITHUB_REF_NAME'\/AOSPMods_Xposed.zip",/' latestStable.json
 
 # module changelog
 echo "**$NEWVERNAME**" > newChangeLog.md
@@ -46,7 +49,7 @@ cat .github/workflowFiles/ReleaseNotesTemplate.md >> releaseNotes.md
 echo "*$NEWVERNAME* released in stable channel  " > telegram.msg
 echo "  " >> telegram.msg
 echo "*Changelog:*  " >> telegram.msg
-cat changeLog.md >> telegram.msg
+cat .github/workflowFiles/FutureChanageLog.md >> telegram.msg
 echo 'TMessage<<EOF' >> $GITHUB_ENV
 cat telegram.msg >> $GITHUB_ENV
 echo 'EOF' >> $GITHUB_ENV
--- .github/workflowFiles/FutureChanageLog.md
+++ .github/workflowFiles/FutureChanageLog.md
@@ -1,8 +1,2 @@
-- [Misc] Added option to adjust media volume steps (Misc)  
-- [App] UI improvements  
-- [Misc] Added option to enable phone call vibration feedbacks  
-- [Updates] Additional package type: Xposed only (Themes, icons and fonts not included)  
-- [App] Added option to choose language  
-  
-- Bugfixes  
+- Updater bugfix  
   

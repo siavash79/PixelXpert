0a7feb3180195d60b1ba61ba218e40901f2a24ac
Siavash <siavash@gmail.com>
2022-08-19 19:48:30 +0430

    CHANGELOG: A13: Fixed statusbar notification count limits (A12 Users: don't install this)

--- app/src/main/java/sh/siava/AOSPMods/utils/NotificationIconContainerOverride.java
+++ app/src/main/java/sh/siava/AOSPMods/utils/NotificationIconContainerOverride.java
@@ -22,10 +22,15 @@ public class NotificationIconContainerOverride {
 
     private static final int MAX_VISIBLE_ICONS_ON_LOCK = 5;
     public static int MAX_STATIC_ICONS = 4;
+    private static final int MAX_ICONS_ON_AOD = 3;
     private static final int MAX_DOTS = 1;
+    private static final int NO_VALUE = Integer.MIN_VALUE;
 
-    public static void calculateIconTranslations(XC_MethodHook.MethodHookParam param) {
+    public static void calculateIconTranslations(XC_MethodHook.MethodHookParam param) { //A12
         FrameLayout thisObject = (FrameLayout) param.thisObject;
+
+        Object mIconStates = getObjectField(thisObject, "mIconStates");
+
         float translationX = (float) callMethod(thisObject, "getActualPaddingStart");
         int firstOverflowIndex = -1;
         int childCount = thisObject.getChildCount();
@@ -37,7 +42,7 @@ public class NotificationIconContainerOverride {
         setObjectField(thisObject, "mFirstVisibleIconState", null);
         for (int i = 0; i < childCount; i++) {
             View view = thisObject.getChildAt(i);
-            Object iconState = callMethod(getObjectField(thisObject, "mIconStates"), "get",view);
+            Object iconState = callMethod(mIconStates, "get", view);
             if (getFloatField(iconState, "iconAppearAmount") == 1.0f) {
                 // We only modify the xTranslation if it's fully inside of the container
                 // since during the transition to the shelf, the translations are controlled
@@ -52,7 +57,7 @@ public class NotificationIconContainerOverride {
                     && getFloatField(iconState, "iconAppearAmount") > 0.0f || i >= maxVisibleIcons;
             boolean noOverflowAfter = i == childCount - 1;
             float drawingScale = getBooleanField(thisObject, "mOnLockScreen") && view.getClass() == StatusBarIconViewClass
-                    ? (float)callMethod(view, "getIconScaleIncreased")
+                    ? (float) callMethod(view, "getIconScaleIncreased")
                     : 1f;
             setObjectField(iconState, "visibleState", getBooleanField(iconState, "hidden")
                     ? STATE_HIDDEN
@@ -76,18 +81,18 @@ public class NotificationIconContainerOverride {
             translationX = getFloatField(thisObject, "mVisualOverflowStart");
             for (int i = firstOverflowIndex; i < childCount; i++) {
                 View view = thisObject.getChildAt(i);
-                Object iconState = callMethod(getObjectField(thisObject,"mIconStates"),"get",view);
-                int dotWidth = getIntField(thisObject,"mStaticDotDiameter") + getIntField(thisObject, "mDotPadding");
+                Object iconState = callMethod(mIconStates, "get", view);
+                int dotWidth = getIntField(thisObject, "mStaticDotDiameter") + getIntField(thisObject, "mDotPadding");
                 setObjectField(iconState, "xTranslation", translationX);
-                if (getIntField(thisObject,"mNumDots") < MAX_DOTS) {
-                    if (getIntField(thisObject,"mNumDots") == 0 && getFloatField(iconState,"iconAppearAmount") < 0.8f) {
+                if (getIntField(thisObject, "mNumDots") < MAX_DOTS) {
+                    if (getIntField(thisObject, "mNumDots") == 0 && getFloatField(iconState, "iconAppearAmount") < 0.8f) {
                         setObjectField(iconState, "visibleState", STATE_ICON);
                     } else {
                         setObjectField(iconState, "visibleState", STATE_DOT);
-                        setObjectField(thisObject, "mNumDots", getIntField(thisObject, "mNumDots")+1);
+                        setObjectField(thisObject, "mNumDots", getIntField(thisObject, "mNumDots") + 1);
                     }
-                    translationX += (getIntField(thisObject,"mNumDots") == MAX_DOTS ? MAX_DOTS * dotWidth : dotWidth)
-                            * getFloatField(iconState,"iconAppearAmount");
+                    translationX += (getIntField(thisObject, "mNumDots") == MAX_DOTS ? MAX_DOTS * dotWidth : dotWidth)
+                            * getFloatField(iconState, "iconAppearAmount");
                     setObjectField(thisObject, "mLastVisibleIconState", iconState);
                 } else {
                     setObjectField(iconState, "visibleState", STATE_HIDDEN);
@@ -95,28 +100,154 @@ public class NotificationIconContainerOverride {
             }
         } else if (childCount > 0) {
             View lastChild = thisObject.getChildAt(childCount - 1);
-            setObjectField(thisObject, "mLastVisibleIconState", callMethod(getObjectField(thisObject, "mIconStates"),"get",lastChild));
-            setObjectField(thisObject, "mFirstVisibleIconState", callMethod(getObjectField(thisObject, "mIconStates"),"get",thisObject.getChildAt(0)));
+
+            setObjectField(thisObject, "mLastVisibleIconState", callMethod(mIconStates, "get", lastChild));
+            setObjectField(thisObject, "mFirstVisibleIconState", callMethod(mIconStates, "get", thisObject.getChildAt(0)));
         }
 
-        if ((boolean)callMethod(thisObject, "isLayoutRtl")) {
+        if ((boolean) callMethod(thisObject, "isLayoutRtl")) {
             for (int i = 0; i < childCount; i++) {
                 View view = thisObject.getChildAt(i);
-                Object iconState = callMethod(getObjectField(thisObject,  "mIconStates"),"get",view);
-                setObjectField(iconState, "xTranslation", thisObject.getWidth() - getFloatField(iconState,"xTranslation") - view.getWidth());
+                Object iconState = callMethod(mIconStates, "get", view);
+                setObjectField(iconState, "xTranslation", thisObject.getWidth() - getFloatField(iconState, "xTranslation") - view.getWidth());
             }
         }
         if (getObjectField(thisObject, "mIsolatedIcon") != null) {
-            Object iconState = callMethod(getObjectField(thisObject, "mIconStates"), "get",getObjectField(thisObject, "mIsolatedIcon"));
+            Object iconState = callMethod(mIconStates, "get", getObjectField(thisObject, "mIsolatedIcon"));
             if (iconState != null) {
                 // Most of the time the icon isn't yet added when this is called but only happening
                 // later
                 int[] mAbsolutePosition = (int[]) getObjectField(thisObject, "mAbsolutePosition");
                 setObjectField(iconState, "xTranslation", getFloatField(getObjectField(thisObject, "mIsolatedIconLocation"), "left") - mAbsolutePosition[0]
-                - (1 - (float)callMethod(getObjectField(thisObject, "mIsolatedIcon"), "getIconScale")) * (int)callMethod(getObjectField(thisObject, "mIsolatedIcon"), "getWidth") / 2.0f);
+                        - (1 - (float) callMethod(getObjectField(thisObject, "mIsolatedIcon"), "getIconScale")) * (int) callMethod(getObjectField(thisObject, "mIsolatedIcon"), "getWidth") / 2.0f);
                 setObjectField(iconState, "visibleState", STATE_ICON);
             }
         }
     }
 
+    public static void calculateIconXTranslations(XC_MethodHook.MethodHookParam param) { //A13
+        FrameLayout thisObject = (FrameLayout) param.thisObject;
+
+        Object mIconStates = getObjectField(thisObject, "mIconStates");
+
+        float translationX = (float) callMethod(thisObject, "getActualPaddingStart");
+        int firstOverflowIndex = -1;
+        int childCount = thisObject.getChildCount();
+        int maxVisibleIcons = getBooleanField(thisObject, "mOnLockScreen") ? MAX_ICONS_ON_AOD :
+                getBooleanField(thisObject, "mIsStaticLayout") ? MAX_STATIC_ICONS : childCount;
+        float layoutEnd = getLayoutEnd(thisObject);
+        setObjectField(thisObject, "mVisualOverflowStart", 0);
+        setObjectField(thisObject, "mFirstVisibleIconState", null);
+        for (int i = 0; i < childCount; i++) {
+            View view = thisObject.getChildAt(i);
+            Object iconState = callMethod(mIconStates,"get", view);
+            if (getFloatField(iconState, "iconAppearAmount") == 1.0f) {
+                // We only modify the xTranslation if it's fully inside of the container
+                // since during the transition to the shelf, the translations are controlled
+                // from the outside
+                setObjectField(iconState, "xTranslation", translationX);
+            }
+            if (getObjectField(thisObject, "mFirstVisibleIconState") == null) {
+                setObjectField(thisObject, "mFirstVisibleIconState", iconState);
+            }
+            int mSpeedBumpIndex = getIntField(thisObject, "mSpeedBumpIndex");
+            boolean forceOverflow = mSpeedBumpIndex != -1 && i >= mSpeedBumpIndex
+                    && getFloatField(iconState, "iconAppearAmount") > 0.0f || i >= maxVisibleIcons;
+            boolean isLastChild = i == childCount - 1;
+            float drawingScale = getBooleanField(thisObject, "mOnLockScreen") && view.getClass().getName().endsWith("StatusBarIconView")
+                    ? getIconScaleIncreased(view)
+                    : 1f;
+            setObjectField(iconState, "visibleState", getBooleanField(iconState, "hidden")
+                    ? STATE_HIDDEN
+                    : STATE_ICON);
+
+            final float overflowDotX = layoutEnd - getIntField(thisObject, "mIconSize");
+            boolean isOverflowing = translationX > overflowDotX;
+
+            if (firstOverflowIndex == -1 && (forceOverflow || isOverflowing)) {
+                firstOverflowIndex = isLastChild && !forceOverflow ? i - 1 : i;
+                setObjectField(thisObject, "mVisualOverflowStart", layoutEnd - getIntField(thisObject, "mIconSize"));
+                if (forceOverflow || getBooleanField(thisObject, "mIsStaticLayout")) {
+                    setObjectField(thisObject, "mVisualOverflowStart", Math.min(translationX, getFloatField(thisObject, "mVisualOverflowStart")));
+                }
+            }
+            translationX += getFloatField(iconState, "iconAppearAmount") * view.getWidth() * drawingScale;
+        }
+        setObjectField(thisObject, "mNumDots", 0);
+        if (firstOverflowIndex != -1) {
+            translationX = getFloatField(thisObject, "mVisualOverflowStart");
+            for (int i = firstOverflowIndex; i < childCount; i++) {
+                View view = thisObject.getChildAt(i);
+                Object iconState = callMethod(mIconStates, "get", view);
+                int dotWidth = getIntField(thisObject, "mStaticDotDiameter") + getIntField(thisObject, "mDotPadding");
+                setObjectField(iconState, "xTranslation", translationX);
+                if (getIntField(thisObject, "mNumDots") < MAX_DOTS) {
+                    if (getIntField(thisObject, "mNumDots") == 0 && getFloatField(iconState, "iconAppearAmount") < 0.8f) {
+                        setObjectField(iconState, "visibleState", STATE_ICON);
+                    } else {
+                        setObjectField(iconState, "visibleState", STATE_DOT);
+                        setObjectField(thisObject, "mNumDots", getIntField(thisObject, "mNumDots") + 1);
+                    }
+                    translationX += (getIntField(thisObject, "mNumDots") == MAX_DOTS ? MAX_DOTS * dotWidth : dotWidth)
+                            * getFloatField(iconState, "iconAppearAmount");
+                    try{
+                        setObjectField(thisObject, "mLastVisibleIconState", iconState);
+                    }catch (Throwable ignored){} //unfortunately classloader can't locate this field
+                } else {
+                    setObjectField(iconState, "visibleState", STATE_HIDDEN);
+                }
+            }
+        } else if (childCount > 0) {
+            View lastChild = thisObject.getChildAt(childCount - 1);
+            try {
+                setObjectField(thisObject, "mLastVisibleIconState", callMethod(mIconStates, "get", lastChild));
+            }catch (Throwable ignored){} //unfortunately classloader can't locate this field
+
+            setObjectField(thisObject, "mFirstVisibleIconState", callMethod(mIconStates, "get", thisObject.getChildAt(0)));
+        }
+        if ((boolean)callMethod(thisObject, "isLayoutRtl")) {
+            for (int i = 0; i < childCount; i++) {
+                View view = thisObject.getChildAt(i);
+                Object iconState = callMethod(mIconStates, "get", view);
+                setObjectField(iconState, "xTranslation", thisObject.getWidth() - getIntField(iconState, "xTranslation") - view.getWidth());
+            }
+        }
+        Object mIsolatedIcon = getObjectField(thisObject, "mIsolatedIcon");
+        if (mIsolatedIcon != null) {
+            Object iconState = callMethod(mIconStates, "get", mIsolatedIcon);
+            if (iconState != null) {
+                // Most of the time the icon isn't yet added when this is called but only happening
+                // later
+                setObjectField(iconState, "visibleState", STATE_ICON);
+                setObjectField(iconState, "xTranslation", getIntField(getObjectField(thisObject, "mIsolatedIconLocation"),"left") - ((int[])getObjectField(thisObject, "mAbsolutePosition"))[0]
+                        - (1 - (int)callMethod(getObjectField(thisObject, "mIsolatedIcon"), "getIconScale")) * (int)callMethod(mIsolatedIcon, "getWidth") / 2.0f);
+            }
+        }
+    }
+
+    //Since classloader can't find these methods, we implement them ourselves
+    private static float getLayoutEnd(View v) {
+        return getActualWidth(v) - getActualPaddingEnd(v);
+    }
+
+    private static int getActualWidth(View v) {
+        int mActualLayoutWidth = getIntField(v, "mActualLayoutWidth");
+        if (mActualLayoutWidth == NO_VALUE) {
+            return v.getWidth();
+        }
+        return mActualLayoutWidth;
+    }
+
+    private static float getActualPaddingEnd(View v) {
+        float mActualPaddingEnd = getFloatField(v, "mActualPaddingEnd");
+        if (mActualPaddingEnd == NO_VALUE) {
+            return v.getPaddingEnd();
+        }
+        return mActualPaddingEnd;
+    }
+    
+    private static float getIconScaleIncreased(View v)
+    {
+        return (float) getIntField(v, "mStatusBarIconDrawingSizeIncreased") / getIntField(v, "mStatusBarIconDrawingSize");
+    }
 }

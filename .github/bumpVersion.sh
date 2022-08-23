2a57c95273fd2abcfde2735ff16f87775b8395fb
Siavash <siavash@gmail.com>
2022-08-24 00:30:23 +0430

    Misc fixes

--- .github/bumpVersion.sh
+++ .github/bumpVersion.sh
@@ -31,7 +31,7 @@ sed -i 's/"zipUrl":.*/"zipUrl": "https:\/\/github.com\/siavash79\/AOSPMods\/rele
 #In-app updater
 sed -i 's/"version":.*/"version": "'$NEWVERNAME'",/' latestStable.json
 sed -i 's/"versionCode":.*/"versionCode": '$NEWVERCODE',/' latestStable.json
-sed -i 's/"zipUrl_FUll":.*/"zipUrl_Full": "https:\/\/github.com\/siavash79\/AOSPMods\/releases\/download\/'$GITHUB_REF_NAME'\/AOSPMods_Full.zip",/' latestStable.json
+sed -i 's/"zipUrl_Full":.*/"zipUrl_Full": "https:\/\/github.com\/siavash79\/AOSPMods\/releases\/download\/'$GITHUB_REF_NAME'\/AOSPMods_Full.zip",/' latestStable.json
 sed -i 's/"zipUrl_Xposed":.*/"zipUrl_Xposed": "https:\/\/github.com\/siavash79\/AOSPMods\/releases\/download\/'$GITHUB_REF_NAME'\/AOSPMods_Xposed.zip"/' latestStable.json
 
 # module changelog

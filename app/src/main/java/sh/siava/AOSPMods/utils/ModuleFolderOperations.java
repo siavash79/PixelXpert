bb52d74333c3e87c9a4082d091542313c0859780
siavash79 <siavash@gmail.com>
2022-11-04 21:49:30 +0330

    CHANGELOG: (Full Version) Fixed font customization. May not be recognizable to everyone

--- app/src/main/java/sh/siava/AOSPMods/utils/ModuleFolderOperations.java
+++ app/src/main/java/sh/siava/AOSPMods/utils/ModuleFolderOperations.java
@@ -7,11 +7,11 @@ public class ModuleFolderOperations {
 		new Thread(() -> {
 			try {
 				if (customFontsEnabled && GSansOverrideEnabled) {
-					Shell.cmd(String.format("cp %s/data/fontz/GSans/*.ttf %s/system/fonts/ && cp %s/data/productz/etc/fonts_customization.xml.NEW %s/system/product/etc/fonts_customization.xml", rootPath, rootPath, rootPath, rootPath)).exec();
+					Shell.cmd(String.format("cp %s/data/etcz/fonts.xml %s/system/etc/ && cp %s/data/fontz/GSans/*.ttf %s/system/fonts/ && cp %s/data/productz/etc/fonts_customization.xml.NEW %s/system/product/etc/fonts_customization.xml", rootPath, rootPath, rootPath, rootPath, rootPath, rootPath)).exec();
 				} else if (customFontsEnabled) {
-					Shell.cmd(String.format("rm -rf %s/system/fonts/*.ttf && cp %s/data/productz/etc/fonts_customization.xml.OLD %s/system/product/etc/fonts_customization.xml && cp -r %s/data/productz/fonts/* %s/system/product/fonts/", rootPath, rootPath, rootPath, rootPath, rootPath)).exec();
+					Shell.cmd(String.format("cp %s/data/etcz/fonts.xml %s/system/etc/ && rm -rf %s/system/fonts/*.ttf && cp %s/data/productz/etc/fonts_customization.xml.OLD %s/system/product/etc/fonts_customization.xml && cp -r %s/data/productz/fonts/* %s/system/product/fonts/", rootPath, rootPath, rootPath, rootPath, rootPath, rootPath, rootPath)).exec();
 				} else {
-					Shell.cmd(String.format("rm -rf %s/system/fonts/*.ttf && rm -f %s/system/product/etc/fonts_customization.xml && rm -rf %s/system/product/fonts/*", rootPath, rootPath, rootPath)).exec();
+					Shell.cmd(String.format("rm -rf %s/system/fonts/*.ttf && rm -f %s/system/product/etc/fonts_customization.xml && rm -rf %s/system/product/fonts/* && rm -rf %s/system/etc/fonts.xml", rootPath, rootPath, rootPath, rootPath)).exec();
 				}
 			} catch (Exception ignored) {
 			}

815395b4339d3474e0b34caac9262ef3439eabb5
Siavash <siavash@gmail.com>
2022-12-12 14:44:53 +0330

    Prepare for 2.4.1

--- .github/bumpVersion.sh
+++ .github/bumpVersion.sh
@@ -1,35 +1,56 @@
 #!/bin/bash
 
 NEWVERCODE=$(($(cat app/build.gradle | grep versionCode | tr -s ' ' | cut -d " " -f 2 | tr -d '\r')+1))
-NEWVERNAME="canary-$NEWVERCODE"
+NEWVERNAME=${GITHUB_REF_NAME/v/}
 
+#Prepare gradle script for build
 sed -i 's/versionCode.*/versionCode '$NEWVERCODE'/' app/build.gradle
 sed -i 's/versionName.*/versionName "'$NEWVERNAME'"/' app/build.gradle
 
+#prepare magisk module props
+#Xposed
 sed -i 's/version=.*/version='$NEWVERNAME'/' MagiskModBase/module.prop
 sed -i 's/versionCode=.*/versionCode='$NEWVERCODE'/' MagiskModBase/module.prop
+#Full
+sed -i 's/version=.*/version='$NEWVERNAME'/' MagiskModAddon/module.prop
+sed -i 's/versionCode=.*/versionCode='$NEWVERCODE'/' MagiskModAddon/module.prop
 
-sed -i 's/"version":.*/"version": "'$NEWVERNAME'",/' latestVersion.json
-sed -i 's/"versionCode":.*/"versionCode": '$NEWVERCODE',/' latestVersion.json
-sed -i 's/"zipUrl":.*/"zipUrl": "https:\/\/nightly.link\/siavash79\/AOSPMods\/actions\/runs\/'$1'\/AOSPMods_Full.zip",/' latestVersion.json
-
-sed -i 's/"version":.*/"version": "'$NEWVERNAME'",/' latestCanary.json
-sed -i 's/"versionCode":.*/"versionCode": '$NEWVERCODE',/' latestCanary.json
-sed -i 's/"zipUrl_Xposed":.*/"zipUrl_Xposed": "https:\/\/nightly.link\/siavash79\/AOSPMods\/actions\/runs\/'$1'\/AOSPMods_Xposed.zip",/' latestCanary.json
-sed -i 's/"zipUrl_Full":.*/"zipUrl_Full": "https:\/\/nightly.link\/siavash79\/AOSPMods\/actions\/runs\/'$1'\/AOSPMods_Full.zip",/' latestCanary.json
-sed -i 's/"zipUrl":.*/"zipUrl": "https:\/\/nightly.link\/siavash79\/AOSPMods\/actions\/runs\/'$1'\/AOSPMods_Full.zip",/' latestCanary.json
+#Prepare magisk module update files of legacy, full and xposed types
+#legacy
+sed -i 's/"version":.*/"version": "'$NEWVERNAME'",/' MagiskModuleUpdate.json
+sed -i 's/"versionCode":.*/"versionCode": '$NEWVERCODE',/' MagiskModuleUpdate.json
+sed -i 's/"zipUrl":.*/"zipUrl": "https:\/\/github.com\/siavash79\/AOSPMods\/releases\/download\/'$GITHUB_REF_NAME'\/AOSPMods_Full.zip",/' MagiskModuleUpdate.json
+#Full
+sed -i 's/"version":.*/"version": "'$NEWVERNAME'",/' MagiskModuleUpdate_Full.json
+sed -i 's/"versionCode":.*/"versionCode": '$NEWVERCODE',/' MagiskModuleUpdate_Full.json
+sed -i 's/"zipUrl":.*/"zipUrl": "https:\/\/github.com\/siavash79\/AOSPMods\/releases\/download\/'$GITHUB_REF_NAME'\/AOSPMods_Full.zip",/' MagiskModuleUpdate_Full.json
+#Xposed
+sed -i 's/"version":.*/"version": "'$NEWVERNAME'",/' MagiskModuleUpdate_Xposed.json
+sed -i 's/"versionCode":.*/"versionCode": '$NEWVERCODE',/' MagiskModuleUpdate_Xposed.json
+sed -i 's/"zipUrl":.*/"zipUrl": "https:\/\/github.com\/siavash79\/AOSPMods\/releases\/download\/'$GITHUB_REF_NAME'\/AOSPMods_Xposed.zip",/' MagiskModuleUpdate_Xposed.json
+#In-app updater
+sed -i 's/"version":.*/"version": "'$NEWVERNAME'",/' latestStable.json
+sed -i 's/"versionCode":.*/"versionCode": '$NEWVERCODE',/' latestStable.json
+sed -i 's/"zipUrl_Full":.*/"zipUrl_Full": "https:\/\/github.com\/siavash79\/AOSPMods\/releases\/download\/'$GITHUB_REF_NAME'\/AOSPMods_Full.zip",/' latestStable.json
+sed -i 's/"zipUrl_Xposed":.*/"zipUrl_Xposed": "https:\/\/github.com\/siavash79\/AOSPMods\/releases\/download\/'$GITHUB_REF_NAME'\/AOSPMods_Xposed.zip"/' latestStable.json
 
 # module changelog
 echo "**$NEWVERNAME**  " > newChangeLog.md
-cat changeLog.md >> newChangeLog.md
+cat .github/workflowFiles/FutureChanageLog.md >> newChangeLog.md
 echo "  " >> newChangeLog.md
-cat CanaryChangelog.md >> newChangeLog.md
-mv  newChangeLog.md CanaryChangelog.md
+cat MagiskChangelog.md >> newChangeLog.md
+mv  newChangeLog.md MagiskChangelog.md
 
-echo "*$NEWVERNAME* released in canary channel  " > telegram.msg
+# release notes
+echo "**Changelog:**  " > releaseNotes.md
+cat .github/workflowFiles/FutureChanageLog.md >> releaseNotes.md
+cat .github/workflowFiles/ReleaseNotesTemplate.md >> releaseNotes.md
+
+echo "*$NEWVERNAME* released in stable channel  " > telegram.msg
 echo "  " >> telegram.msg
 echo "*Changelog:*  " >> telegram.msg
-cat changeLog.md >> telegram.msg
+cat .github/workflowFiles/FutureChanageLog.md >> telegram.msg
 echo 'TMessage<<EOF' >> $GITHUB_ENV
 cat telegram.msg >> $GITHUB_ENV
+echo >> $GITHUB_ENV
 echo 'EOF' >> $GITHUB_ENV
--- .github/make_zip.sh
+++ .github/make_zip.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-mkdir -p output/full output/xposed
+mkdir -p output
 cp app/build/outputs/apk/release/AOSPMods-signed.apk MagiskModBase/system/priv-app/AOSPMods/AOSPMods.apk
 cd MagiskModBase;
 LITE_FILENAME="AOSPMods_Xposed.zip"
 FULL_FILENAME="AOSPMods_Full.zip"
 
 echo 1 > build.type
-zip -r ../output/xposed/$LITE_FILENAME *;
+zip -r ../output/$LITE_FILENAME *;
 
-cp -r ../MagiskModAddon/* ./
+#we'll need this file to return safely
+mkdir ../tmp
+mv module.prop ../tmp/module.prop
+
+cp -rf ../MagiskModAddon/* ./
 echo 0 > build.type
 
-zip -r ../output/full/$FULL_FILENAME *;
+zip -r ../output/$FULL_FILENAME *;
+
+#bring xposed mod prop back for commit
+rm -rf module.prop
+mv ../tmp/module.prop module.prop

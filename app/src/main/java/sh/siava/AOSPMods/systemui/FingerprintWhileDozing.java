0b00e424a3ac726eba300d65bb2c1c18ea55252f
Siavash <siavash@gmail.com>
2023-04-25 12:23:31 +0330

    simplified FingerprintWhileDozing

--- app/src/main/java/sh/siava/AOSPMods/systemui/FingerprintWhileDozing.java
+++ app/src/main/java/sh/siava/AOSPMods/systemui/FingerprintWhileDozing.java
@@ -1,9 +1,7 @@
 package sh.siava.AOSPMods.systemui;
 
 import static de.robv.android.xposed.XposedBridge.hookAllMethods;
-import static de.robv.android.xposed.XposedHelpers.callMethod;
 import static de.robv.android.xposed.XposedHelpers.findClass;
-import static de.robv.android.xposed.XposedHelpers.findFieldIfExists;
 import static de.robv.android.xposed.XposedHelpers.getBooleanField;
 import static sh.siava.AOSPMods.XPrefs.Xprefs;
 
@@ -39,29 +37,15 @@ public class FingerprintWhileDozing extends XposedModPack {
 
 		Class<?> KeyguardUpdateMonitorClass = findClass("com.android.keyguard.KeyguardUpdateMonitor", lpparam.classLoader);
 
-		String keyguardShowingField = (findFieldIfExists(KeyguardUpdateMonitorClass, "mKeyguardShowing") != null) ? "mKeyguardShowing" : "mKeyguardIsVisible"; // 13 QPR1 vs QPR2
-
 		hookAllMethods(KeyguardUpdateMonitorClass,
 				"shouldListenForFingerprint", new XC_MethodHook() {
 					@Override
-					protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+					protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
 						if (fingerprintWhileDozing) return;
-						boolean currentResult = (boolean) param.getResult();
-						if (currentResult) {
-							boolean userDoesNotHaveTrust = !(boolean) callMethod(param.thisObject,
-									"getUserHasTrust",
-									callMethod(param.thisObject, "getCurrentUser"));
 
-							boolean shouldlisten2 =
-									(getBooleanField(param.thisObject, keyguardShowingField)
-											|| getBooleanField(param.thisObject, "mBouncerIsOrWillBeShowing")
-											|| (boolean) callMethod(param.thisObject, "shouldListenForFingerprintAssistant")
-											|| (getBooleanField(param.thisObject, "mKeyguardOccluded") && getBooleanField(param.thisObject, "mIsDreaming")))
-											&& getBooleanField(param.thisObject, "mDeviceInteractive") && !getBooleanField(param.thisObject, "mGoingToSleep") && !getBooleanField(param.thisObject, "mKeyguardGoingAway")
-											|| (getBooleanField(param.thisObject, "mKeyguardOccluded") && userDoesNotHaveTrust
-											&& (getBooleanField(param.thisObject, "mOccludingAppRequestingFp") || (boolean) param.args[0]));
-
-							param.setResult(shouldlisten2);
+						if(!getBooleanField(param.thisObject, "mDeviceInteractive"))
+						{
+							param.setResult(false);
 						}
 					}
 				});

a33bedd04262d736c1d33b56296d184abff8d218
Siavash <siavash@gmail.com>
2023-06-19 08:19:30 +0330

    dependency update

--- app/src/main/java/sh/siava/AOSPMods/modpacks/ModPacks.java
+++ app/src/main/java/sh/siava/AOSPMods/modpacks/ModPacks.java
@@ -49,6 +49,7 @@ public class ModPacks {
 	{
 		ArrayList<Class> modPacks = new ArrayList<>();
 
+		//SystemUI
 		modPacks.add(NotificationExpander.class);
 		modPacks.add(QSTileGrid.class);
 		modPacks.add(BrightnessSlider.class);
@@ -68,27 +69,33 @@ public class ModPacks {
 		modPacks.add(GestureNavbarManager.class);
 		modPacks.add(QSFooterTextManager.class);
 		modPacks.add(ScreenshotMuter.class);
-		modPacks.add(ScreenOffKeys.class);
-		modPacks.add(TaskbarActivator.class);
 		modPacks.add(KeyGuardPinScrambler.class);
-		modPacks.add(OverScrollDisabler.class);
 		modPacks.add(FingerprintWhileDozing.class);
 		modPacks.add(StatusbarSize.class);
-		modPacks.add(ScreenRotation.class);
-		modPacks.add(CallVibrator.class);
 		modPacks.add(FlashLightLevel.class);
 		modPacks.add(CustomNavGestures.class);
-		modPacks.add(PhoneWindowManager.class);
-		modPacks.add(BrightnessRange.class);
-		modPacks.add(ClearAllButtonMod.class);
 		modPacks.add(NotificationManager.class);
-		modPacks.add(PackageManager.class);
-		modPacks.add(AOSPModsIconUpdater.class);
 		modPacks.add(VolumeTile.class);
+		//Telecom
+		modPacks.add(CallVibrator.class);
+		//Framework
+		modPacks.add(PackageManager.class);
+		modPacks.add(BrightnessRange.class);
+		modPacks.add(PhoneWindowManager.class);
+		modPacks.add(ScreenRotation.class);
+		modPacks.add(ScreenOffKeys.class);
 		modPacks.add(HotSpotController.class);
+		//Launcher
+		modPacks.add(TaskbarActivator.class);
+		modPacks.add(ClearAllButtonMod.class);
+		modPacks.add(AOSPModsIconUpdater.class);
 		modPacks.add(FeatureFlags.class);
+		//Settings
 		modPacks.add(AppCloneEnabler.class);
+		//Dialer
 		modPacks.add(RecordingMessage.class);
+		//All Apps
+		modPacks.add(OverScrollDisabler.class);
 
 		return modPacks;
 	}

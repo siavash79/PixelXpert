315f3037550e5e86ab13f5067a0282aadc5f701d
DrDisagree <crazymahmud08@gmail.com>
2023-08-11 16:04:19 +0600

    Prevent device from rebooting while importing settings

--- app/src/main/java/sh/siava/AOSPMods/modpacks/XPrefs.java
+++ app/src/main/java/sh/siava/AOSPMods/modpacks/XPrefs.java
@@ -20,20 +20,18 @@ public class XPrefs {
 
 	private static final OnSharedPreferenceChangeListener listener = (sharedPreferences, key) -> loadEverything(packageName, key);
 
-	public static void init(Context context)
-	{
+	public static void init(Context context) {
 		packageName = context.getPackageName();
 		Xprefs = new RemotePreferences(context, BuildConfig.APPLICATION_ID, BuildConfig.APPLICATION_ID + "_preferences", true);
 		log("AOSPMods Version: " + BuildConfig.VERSION_NAME);
 		try {
 			log("AOSPMods Records: " + Xprefs.getAll().keySet().size());
-		}catch (Throwable ignored){}
+		} catch (Throwable ignored) {}
 		Xprefs.registerOnSharedPreferenceChangeListener(listener);
 	}
 
 	public static void loadEverything(String packageName, String... key) {
-		if(key.length > 0
-				&& Constants.PREF_UPDATE_EXCLUSIONS.stream().anyMatch(exclusion -> key[0].startsWith(exclusion)))
+		if (key.length > 0 && (key[0] == null || Constants.PREF_UPDATE_EXCLUSIONS.stream().anyMatch(exclusion -> key[0].startsWith(exclusion))))
 			return;
 
 		setPackagePrefs(packageName);

373f3e3c984c44bfd841d3f19c2dec7ee45ab195
DrDisagree <crazymahmud08@gmail.com>
2023-11-21 01:22:58 +0600

    Added Crowdin in settings about section

--- app/src/main/java/sh/siava/pixelxpert/ui/activities/SettingsActivity.java
+++ app/src/main/java/sh/siava/pixelxpert/ui/activities/SettingsActivity.java
@@ -948,6 +948,19 @@ public class SettingsActivity extends BaseActivity implements PreferenceFragment
 						return true;
 					});
 
+			findPreference("CrowdinProject").setSummary(getString(R.string.crowdin_summary, getString(R.string.app_name)));
+			findPreference("CrowdinProject")
+					.setOnPreferenceClickListener(preference -> {
+						try {
+							Intent intent = new Intent(Intent.ACTION_VIEW);
+							intent.setData(Uri.parse("https://crowdin.com/project/aospmods"));
+							startActivity(intent);
+						} catch (Exception ignored) {
+							Toast.makeText(getContext(), getString(R.string.browser_not_found), Toast.LENGTH_SHORT).show();
+						}
+						return true;
+					});
+
 			findPreference("UsageWiki")
 					.setOnPreferenceClickListener(preference -> {
 						try {

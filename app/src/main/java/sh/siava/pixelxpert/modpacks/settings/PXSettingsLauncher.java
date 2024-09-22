c61ad29b8ab432b6343e7157ae7a8bc969da5278
Siavash <siavash@gmail.com>
2024-09-22 14:04:34 +0330

    CHANGELOG: A15QPR1B2: Compatibility with new settings app

--- app/src/main/java/sh/siava/pixelxpert/modpacks/settings/PXSettingsLauncher.java
+++ app/src/main/java/sh/siava/pixelxpert/modpacks/settings/PXSettingsLauncher.java
@@ -3,13 +3,19 @@ package sh.siava.pixelxpert.modpacks.settings;
 import static de.robv.android.xposed.XposedBridge.hookAllMethods;
 import static de.robv.android.xposed.XposedHelpers.callMethod;
 import static de.robv.android.xposed.XposedHelpers.findClass;
-import static de.robv.android.xposed.XposedHelpers.getObjectField;
+import static de.robv.android.xposed.XposedHelpers.findClassIfExists;
+import static de.robv.android.xposed.XposedHelpers.setObjectField;
 import static sh.siava.pixelxpert.modpacks.XPrefs.Xprefs;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 
 import androidx.core.content.res.ResourcesCompat;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
@@ -26,10 +32,11 @@ public class PXSettingsLauncher extends XposedModPack {
 
 	private static boolean PXInSettings = true;
 
+	private boolean mNewSettings = true;
+
 	public PXSettingsLauncher(Context context) {
 		super(context);
 	}
-
 
 	@Override
 	public void updatePrefs(String... Key) {
@@ -45,21 +52,25 @@ public class PXSettingsLauncher extends XposedModPack {
 	public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpParam) throws Throwable {
 		Class<?> HomepagePreferenceClass = findClass("com.android.settings.widget.HomepagePreference", lpParam.classLoader);
 		Class<?> TopLevelSettingsClass = findClass("com.android.settings.homepage.TopLevelSettings", lpParam.classLoader);
+		Class<?> OnPreferenceClickListenerInterface = findClass("androidx.preference.Preference$OnPreferenceClickListener", lpParam.classLoader);
 
-		hookAllMethods(TopLevelSettingsClass, "onPreferenceTreeClick", new XC_MethodHook() {
+		Class<?> PreferenceCategoryClass = findClassIfExists("androidx.preference.PreferenceCategory", lpParam.classLoader);
+		Class<?> PreferenceManagerClass = findClassIfExists("androidx.preference.PreferenceManager", lpParam.classLoader);
+
+		hookAllMethods(TopLevelSettingsClass, "getPreferenceScreenResId", new XC_MethodHook() {
 			@Override
-			protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-				if(ResourceManager.modRes.getString(R.string.app_name).equals(getObjectField(param.args[0], "mTitle")))
-				{
-					param.setResult(true);
+			protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+				@SuppressLint("DiscouragedApi")
+				int oldResName = mContext.getResources().getIdentifier("top_level_settings", "xml", mContext.getPackageName());
 
-					Intent intent = mContext.getPackageManager().getLaunchIntentForPackage(BuildConfig.APPLICATION_ID);
-					mContext.startActivity(intent);
+				if(param.getResult().equals(oldResName))
+				{
+					mNewSettings = false;
 				}
 			}
 		});
-
 		hookAllMethods(TopLevelSettingsClass, "onCreateAdapter", new XC_MethodHook() {
+			@SuppressLint("DiscouragedApi")
 			@Override
 			protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
 				if (PXInSettings) {
@@ -70,11 +81,54 @@ public class PXSettingsLauncher extends XposedModPack {
 									R.drawable.ic_notification_foreground,
 									mContext.getTheme()));
 					callMethod(PXPreference, "setTitle", ResourceManager.modRes.getString(R.string.app_name));
-					callMethod(PXPreference, "setOrder", 9999);
 
-					callMethod(param.args[0], "addPreference", PXPreference);
+					Object onClickListener = Proxy.newProxyInstance(
+							OnPreferenceClickListenerInterface.getClassLoader(),
+							new Class[] { OnPreferenceClickListenerInterface },
+							new PXClickListener());
+
+					setObjectField(PXPreference, "mOnClickListener", onClickListener);
+
+					if(mNewSettings)
+					{
+						callMethod(PXPreference, "setSummary", ResourceManager.modRes.getString(R.string.xposed_desc));
+						Object PXPreferenceCategory = PreferenceCategoryClass.getConstructor(Context.class).newInstance(mContext);
+
+						setObjectField(PXPreferenceCategory,
+								"mPreferenceManager",
+								PreferenceManagerClass.getConstructor(Context.class).newInstance(mContext));
+						callMethod(PXPreferenceCategory, "setOrder", 9999);
+
+						int layoutID = mContext.getResources().getIdentifier(
+								"settingslib_preference_category_no_title",
+								"layout",
+								mContext.getPackageName());
+
+						if(layoutID != 0)
+							callMethod(PXPreferenceCategory, "setLayoutResource",layoutID);
+
+						callMethod(PXPreferenceCategory, "addPreference", PXPreference);
+
+						callMethod(param.args[0], "addPreference", PXPreferenceCategory);
+					}
+					else
+					{
+						callMethod(PXPreference, "setOrder", 9999);
+						callMethod(param.args[0], "addPreference", PXPreference);
+					}
 				}
 			}
 		});
+	}
+
+	class PXClickListener implements InvocationHandler {
+		/** @noinspection SuspiciousInvocationHandlerImplementation*/
+		@Override
+		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+			Intent intent = mContext.getPackageManager().getLaunchIntentForPackage(BuildConfig.APPLICATION_ID);
+			mContext.startActivity(intent);
+
+			return true;
+		}
 	}
 }

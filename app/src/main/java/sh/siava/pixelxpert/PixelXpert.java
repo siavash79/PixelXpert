2cbe1c49af922f6a88f6cb8cde4f6f5c0a7ef854
Siavash <siavash@gmail.com>
2024-09-23 10:45:25 +0330

    CHANGELOG: Improved app startup duration

--- app/src/main/java/sh/siava/pixelxpert/PixelXpert.java
+++ app/src/main/java/sh/siava/pixelxpert/PixelXpert.java
@@ -1,35 +1,93 @@
 package sh.siava.pixelxpert;
 
 import android.app.Application;
-import android.content.Context;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Looper;
 
 import com.google.android.material.color.DynamicColors;
+import com.topjohnwu.superuser.Shell;
+import com.topjohnwu.superuser.ipc.RootService;
 
-import java.lang.ref.WeakReference;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import sh.siava.pixelxpert.service.RootProvider;
 
 public class PixelXpert extends Application {
 
+	/** @noinspection unused*/
+	private static final String TAG = "PixelXpertSingleton";
+	private final Handler mainThreadHandler = new Handler(Looper.getMainLooper());
+
 	private static PixelXpert instance;
-	private static WeakReference<Context> contextReference;
+	private boolean mCoreRootServiceBound = false;
+	public final CountDownLatch mRootServiceConnected = new CountDownLatch(1);
+
+	private ServiceConnection mCoreRootServiceConnection;
+
 
 	public void onCreate() {
 		super.onCreate();
 		instance = this;
-		contextReference = new WeakReference<>(getApplicationContext());
+
+		tryConnectRootService();
 		DynamicColors.applyToActivitiesIfAvailable(this);
 	}
 
-	public static Context getAppContext() {
-		if (contextReference == null || contextReference.get() == null) {
-			contextReference = new WeakReference<>(PixelXpert.getInstance().getApplicationContext());
-		}
-		return contextReference.get();
-	}
-
-	private static PixelXpert getInstance() {
+	public static PixelXpert get() {
 		if (instance == null) {
 			instance = new PixelXpert();
 		}
 		return instance;
 	}
+
+	public boolean isCoreRootServiceBound() {
+		return mCoreRootServiceBound;
+	}
+
+	public boolean hasRootAccess()
+	{
+		return Shell.getShell().isRoot();
+	}
+
+	public void tryConnectRootService()
+	{
+		new Thread(() -> {
+			for (int i = 0; i < 2; i++) {
+				if (connectRootService())
+					break;
+			}
+		}).start();
+	}
+
+	private boolean connectRootService() {
+		try {
+			// Start RootService connection
+			Intent intent = new Intent(this, RootProvider.class);
+			mCoreRootServiceConnection = new ServiceConnection() {
+				@Override
+				public void onServiceConnected(ComponentName name, IBinder service) {
+					mCoreRootServiceBound = true;
+					mRootServiceConnected.countDown();
+				}
+
+				@Override
+				public void onServiceDisconnected(ComponentName name) {
+					mCoreRootServiceBound = false;
+					mRootServiceConnected.countDown();
+				}
+			};
+
+			mainThreadHandler.post(() -> RootService.bind(intent, mCoreRootServiceConnection));
+
+			return mRootServiceConnected.await(5, TimeUnit.SECONDS);
+		} catch (Exception ignored) {
+			return false;
+		}
+	}
+
 }

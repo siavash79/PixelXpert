af4fb5be787ea60f7891f9c2594db85946911024
Mygod <contact-git@mygod.be>
2026-05-16 14:11:39 -0400

    Fix HideNavigationBarInsets libxposed imports

--- app/src/main/java/sh/siava/pixelxpert/xposed/modpacks/launcher/HideNavigationBarInsets.java
+++ app/src/main/java/sh/siava/pixelxpert/xposed/modpacks/launcher/HideNavigationBarInsets.java
@@ -9,11 +9,11 @@ import android.view.WindowManager;
 import java.lang.reflect.Array;
 
 import de.robv.android.xposed.XposedHelpers;
-import de.robv.android.xposed.callbacks.XC_LoadPackage;
+import io.github.libxposed.api.XposedModuleInterface;
 import sh.siava.pixelxpert.xposed.XposedModPack;
 import sh.siava.pixelxpert.xposed.annotations.LauncherModPack;
 import sh.siava.pixelxpert.xposed.utils.SystemUtils;
-import sh.siava.pixelxpert.xposed.utils.toolkit.ReflectedClass;
+import sh.siava.pixelxpert.xposed.utils.reflection.ReflectedClass;
 
 @LauncherModPack
 public class HideNavigationBarInsets extends XposedModPack {
@@ -33,7 +33,7 @@ public class HideNavigationBarInsets extends XposedModPack {
 
     @SuppressLint("DiscouragedApi")
     @Override
-    public void onPackageLoaded(XC_LoadPackage.LoadPackageParam lpParam) throws Throwable {
+    public void onPackageLoaded(XposedModuleInterface.PackageReadyParam PRParam) throws Throwable {
         ReflectedClass TaskbarActivityContextClass = ReflectedClass.of("com.android.launcher3.taskbar.TaskbarActivityContext");
         TaskbarActivityContextClass
                 .before("notifyUpdateLayoutParams")

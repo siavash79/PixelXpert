af44d59f19b9082d28b2768e293006c1abd1d886
Dimariqe <dimariqe@gmail.com>
2026-06-09 19:26:53 +0700

    Use release APK in make_debug_zip.sh

--- .github/make_debug_zip.sh
+++ .github/make_debug_zip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir output
-cp app/build/outputs/apk/debug/PixelXpert-signed.apk MagiskModBase/system/priv-app/PixelXpert/PixelXpert.apk
+cp app/build/outputs/apk/release/PixelXpert.apk MagiskModBase/system/priv-app/PixelXpert/PixelXpert.apk
 cd MagiskModBase
 
 FILENAME="PixelXpert.zip"
